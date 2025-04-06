a8329c6c9d94defc9c8e8b6ebd8d860015c3f698
thesquidgrid <skbuchman23@gmail.com>
2025-04-06 16:46:16 -0400

    my js wasn't updated :(

--- githubCommit.js
+++ githubCommit.js
@@ -1,6 +1,6 @@
 fetch('https://api.github.com/repos/TheSquidGrid/koshkaKorner/commits?per_page=1')
   .then(res => res.json())
   .then(res => {
-    document.getElementById('message').innerHTML = res[0].commit.message
-    //document.getElementById('date').innerHTML = res[0].commit.date
+    document.getElementById('message').innerHTML = res[0].commit.message;
+    document.getElementById('date').innerHTML = res[0].commit.author.date.slice(0,10);
   })
