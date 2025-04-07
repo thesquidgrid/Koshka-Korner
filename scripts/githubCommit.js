7c8e66ff5f445c41dbdb73673261165b2c50f894
thesquidgrid <skbuchman23@gmail.com>
2025-04-07 02:59:31 -0400

    edited long messages

--- scripts/githubCommit.js
+++ scripts/githubCommit.js
@@ -1,6 +1,10 @@
 fetch('https://api.github.com/repos/TheSquidGrid/Koshka-Korner/commits?per_page=1')
   .then(res => res.json())
   .then(res => {
-    document.getElementById('message').innerHTML = res[0].commit.message;
+    var msg = res[0].commit.message;
+    if(msg.length > 30){
+        msg = msg.slice(0, 30) + "...";
+    }
+    document.getElementById('message').innerHTML = msg;
     document.getElementById('date').innerHTML = res[0].commit.author.date.slice(0,10);
   })
