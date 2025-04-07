0beea1acedd32a917213c60dc4e79128eeceb3f6
thesquidgrid <skbuchman23@gmail.com>
2025-04-07 03:03:42 -0400

    editing github messsage on the top to look nicer

--- scripts/githubCommit.js
+++ scripts/githubCommit.js
@@ -2,8 +2,8 @@ fetch('https://api.github.com/repos/TheSquidGrid/Koshka-Korner/commits?per_page=
   .then(res => res.json())
   .then(res => {
     var msg = res[0].commit.message;
-    if(msg.length > 30){
-        msg = msg.slice(0, 30) + "...";
+    if(msg.length > 25){
+        msg = msg.slice(0, 25) + "...";
     }
     document.getElementById('message').innerHTML = msg;
     document.getElementById('date').innerHTML = res[0].commit.author.date.slice(0,10);
