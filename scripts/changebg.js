634685e93cb0df6a5f1895b668e07fe58e56816e
thesquidgrid <skbuchman23@gmail.com>
2025-04-13 02:59:09 -0400

    scuffed as fuck ((parseint????)) other wise it works

--- scripts/changebg.js
+++ scripts/changebg.js
@@ -1,22 +1,25 @@
 function changeTabColor() {
-    console.log("Changing tab color...");  
+    console.log("Changing tab color...");
     $(".ui-tabs-panel").css("background-color", "");
 
-    var activeTab = 1;
-    console.log("Active tab index: " + activeTab);  
+    var activeTabId = $(".ui-tabs-panel:not(.ui-tabs-hide)").attr('id');
+    
+    var activeTabIndex = parseInt(activeTabId.split('-')[1], 10);
+
+    console.log("Active tab index: " + activeTabIndex);  
 
     var colors = ["#ff000d", "#ff9100", "#eeff00", "#1eff00", "#795bff", "#ca42ff", "#ff00aa"];
     
-    $(".ui-tabs-panel").eq(activeTab).css("background-color", colors[activeTab]);
-  }
+    $(".ui-tabs-panel").eq(activeTabIndex - 1).css("background-color", colors[activeTabIndex - 1]);
+}
 
-  // When a tab is clicked, update the color
-  $(document).ready(function() {
-    // Attach the event to tab clicks
+$(document).ready(function() {
     $("#tabsbar a").click(function() {
-      $("#tabsbar li").removeClass("ui-state-active");
-      $(this).parent().addClass("ui-state-active");
+        $("#tabsbar li").removeClass("ui-state-active");
+        $(this).parent().addClass("ui-state-active");
 
-      changeTabColor();
+        changeTabColor();
     });
-  });
+
+    changeTabColor();
+});
